0043fc0d65dfad03a61baed34c95f4614e96a4c1
auddy <soumyadeepauddy1999@gmail.com>
2022-01-18 20:22:51 +0530

    added camera test

---
+++ cameraTest.py
@@ -0,0 +1,17 @@
+import cv2
+import random
+
+def main():
+    cap = cv2.VideoCapture(0)
+
+    while True:
+        success,img = cap.read()
+
+        cv2.putText(img, str(random.randint(1,10)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+
+        cv2.imshow('image1',img)
+        cv2.waitKey(5)
+
+
+if __name__=="__main__":
+    main()
