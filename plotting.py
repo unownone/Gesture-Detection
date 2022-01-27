c09a473c936cf7b1c4abd99fd1d145c419f82e05
auddy <soumyadeepauddy1999@gmail.com>
2022-01-27 19:33:02 +0530

    graph plot separate fingers

--- plotting.py
+++ plotting.py
@@ -4,17 +4,14 @@ import mediapipe as mp
 import modules.HandTrackingModule as htm
 import numpy as np
 import matplotlib.pyplot as plt
-import seaborn as sns
 
 def main():
     pTime = 0
     cTime = 0
     cap=cv2.VideoCapture(0)
     detector=htm.handDetector()
-    graphPlotsX = []
-    graphPlotsY = []
-    thumbX = []
-    thumbY = []
+    graphPlotX = [[] for i in range(5)]
+    graphPlotY = [[] for i in range(5)]
 
     while True:
         success,img=cap.read()
@@ -23,12 +20,22 @@ def main():
         
         if len(lmlist) != 0:
             for i in lmlist:
-                if i[0]!=4:
-                    graphPlotsX.append(-i[1])
-                    graphPlotsY.append(-i[2])
+                if i[0]<=4:
+                    graphPlotX[0].append(-i[1])
+                    graphPlotY[0].append(-i[2])
+                elif i[0]<=8:
+                    graphPlotX[1].append(-i[1])
+                    graphPlotY[1].append(-i[2])
+                elif i[0]<=12:
+                    graphPlotX[2].append(-i[1])
+                    graphPlotY[2].append(-i[2])
+                elif i[0]<=16:
+                    graphPlotX[3].append(-i[1])
+                    graphPlotY[3].append(-i[2])
                 else:
-                    thumbX.append(-i[1])
-                    thumbY.append(-i[2])
+                    graphPlotX[4].append(-i[1])
+                    graphPlotY[4].append(-i[2])
+
                 print(str(i[0]) + ": (" + str(i[1]) + "," + str(i[2]) + ")\t")
             
             chosenIndex = 4
@@ -49,9 +56,11 @@ def main():
         if keyPressed == ord(chr(27)):
             break
 
-    plt.scatter(graphPlotsX,graphPlotsY)
-    plt.scatter(thumbX,thumbY)
-    #print(type(l))
+    plt.scatter(graphPlotX[0],graphPlotY[0])
+    plt.scatter(graphPlotX[1],graphPlotY[1])
+    plt.scatter(graphPlotX[2],graphPlotY[2])
+    plt.scatter(graphPlotX[3],graphPlotY[3])
+    plt.scatter(graphPlotX[4],graphPlotY[4])
     plt.show()
 
 if __name__=="__main__":
