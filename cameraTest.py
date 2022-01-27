099398795b73972cc88cb4508447e84bdbb580d3
MAYANK RAJ <rajstarts35325@gmail.com>
2022-01-27 17:56:33 +0530

    Update cameraTest.py

--- cameraTest.py
+++ cameraTest.py
@@ -6,10 +6,15 @@ def main():
 
     while True:
         success,img = cap.read()
+        t_lower = 200  # Lower Threshold
+        t_upper = 150  # Upper threshold
+        # img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+        # Applying the Canny Edge filter
+        edge = cv2.Canny(img, t_lower, t_upper)
 
-        cv2.putText(img, str(random.randint(1,10)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
+        # cv2.putText(edge, str(random.randint(1,10)), (10,70), cv2.FONT_HERSHEY_PLAIN, 3, (255,0,255), 3)
 
-        cv2.imshow('image1',img)
+        cv2.imshow('image1',edge)
         cv2.waitKey(5)
 
 
